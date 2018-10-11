d7e0724b76798d67bab592d458ede02912bf2bd0
Mirza Leka <mirza_376@hotmail.com>
2018-10-11 23:58:49 +0200

    Added event listeners

--- Lesson 1/eventListeners.js
+++ Lesson 1/eventListeners.js
@@ -12,6 +12,8 @@ p.addEventListener('click', function () { div.style.background = "pink"; } );
 
 // p.onclick(function () { div.style.background = "pink"; } );
 
+
+/* Pros and Cons */
 // bad side of the syntax above is that you can only have one listener and every next listener will overwrite the previous,
 // unlike event listeners where you can have as many listeners as you want
 // onclick doesn't support capture either
@@ -20,3 +22,30 @@ p.addEventListener('click', function () { div.style.background = "pink"; } );
 
 
 // Example of events: click, focus, blur, mouseover, mouseout, etc.
+
+
+/* Bubbling vs Recapturing */
+
+// if we have two elements, one inside another, we can choose which element to run first.
+        // like if we put state to recapturing (false), clicking on inner tag will first trigger event for the inner element
+        // and then trigger the outer element
+
+        // opposite of this is bubbling (true), which first triggers outer element and then it triggers inner element
+
+
+        p.addEventListener('click', addText, false);
+
+        div.addEventListener('click', addText2, false);
+
+        // two elements call 2 different functions. once clicked on div, it will triger only div tag
+        // but if you click on <p> due to recapturing (last argument is set to false), <p> will call the function first, then <div>
+
+    function addText() {
+        p.textContent += " Paragraph Text.";
+    }
+
+    function addText2() {
+        p.textContent += " Div Container Text.";
+    }
+
+        // if we set options to bubbling (true), it will be the opposite
---
+++ Lesson 1/tryAndCatch.js
@@ -0,0 +1,23 @@
+
+/* Try and Catch */
+
+// Try and catch is used to handle runtime errors. That means code will run but it will fail at some point, like we dividing with zero
+    // Parse time errors will prevent code from executing at all and will crash at start, like opening { and not closing it
+
+// There are 4 principles of error handling
+    // 1) try
+    // 2) catch
+    // 3) throw
+    // 4) finnaly
+
+// Example
+
+    // try{
+
+    //     console.log("Start of try runs");
+
+    //     unicycle; // is not defined, thus this will fail
+
+    //     console.log("End of try runs -- will never be executed");
+
+    // }
