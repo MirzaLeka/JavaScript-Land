b864efc3ebce6f80eaaeab748fae2dcd895607ca
Mirza Leka <mirza_376@hotmail.com>
2018-10-23 00:13:59 +0200

    Fixing all errors in lesson 2

--- Lesson2/introToModularJS.js
+++ Lesson2/introToModularJS.js
@@ -1,55 +1,57 @@
 const people = (function () {
 
-        let people =  [];
+    let people = [];
 
-        // cache DOM
-        let el = document.querySelector("#peopleModule");
-        let $el = $("#peopleModule"); // we can only call jquery functions with $variables and $(element)
-        let button = this.el.querySelector('button');
-        let input = this.el.querySelector("input");
-        let ul = this.el.querySelector("ul");
-        let $ul = this.$el.find("ul"); // jquery alternative to delegate event
-        let template = this.el.querySelector("#people-template").innerHTML;
+    // cache DOM
+    let el = document.querySelector("#peopleModule");
+    let $el = $("#peopleModule"); // we can only call jquery functions with $variables and $(element)
+    let button = el.querySelector('button');
+    let input = el.querySelector("input");
+    let ul = el.querySelector("ul");
+    let $ul = $el.find("ul"); // jquery alternative to delegate event
+    let template = el.querySelector("#people-template").innerHTML;
 
-        // bind events -- users can't access this
-        button.addEventListener('click', addPerson); //no need to bind this. it's not an object anymore
-        $ul.delegate("i.del", "click", deletePerson);
+    // bind events -- users can't access this
+    button.addEventListener('click', addPerson); //no need to bind this. it's not an object anymore
+    $ul.delegate("i.del", "click", deletePerson);
 
-        _render(); // added _ to easier recognize the private variable
+    _render(); // added _ to easier recognize the private variable
 
-        let _render = () => { // renders HTML
-            let data = {
-                people: this.people
-            };
-            this.ul.innerHTML = Mustache._render(this.template, data);
-        }
-        let addPerson = (value) => { // adds elements to array
-            // we need to check if value of person's name is string
+    function _render () { // renders HTML
+        let data = {
+            people
+        };
+        ul.innerHTML = Mustache.render(template, data); // this is not the same as _render function
+    }
 
-            let name = (typeof value === "string") ? value : input.value;
+    function addPerson (value) { // adds elements to array
+        // we need to check if value of person's name is string
 
-            this.people.push(name); // push value from input or function argument
-            this._render();
-            this.input.value = '';
-        }
-        let deletePerson = (event) => {
-            let i;
-            if (typeof event === "number") {
-                i = event; // if we pass in index as number, delete that index
-            } else { // else delete onclick (x button)
-                let $remove = $(e.target).closest("li");
-                let i = this.$ul.find('li').index($remove);   
-            }
+        let name = (typeof value === "string") ? value : input.value;
 
-            this.people.splice(i, 1);
-            this._render();
+        people.push(name); // push value from input or function argument
+        _render();
+        input.value = '';
+    }
+
+    function deletePerson (event) {
+        let i;
+        if (typeof event === "number") {
+            i = event; // if we pass in index as number, delete that index
+        } else { // else delete onclick (x button)
+            let $remove = $(event.target).closest("li");
+            let i = $ul.find('li').index($remove);
         }
 
-        return { // so user only can control the two methods that we're returning
-            addPerson,
-            deletePerson
-        }
-    
+        people.splice(i, 1);
+        _render();
+    }
+
+    return { // so user only can control the two methods that we're returning
+        addPerson,
+        deletePerson
+    }
+
 })();
 
 
