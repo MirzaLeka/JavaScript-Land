8a798be84bdef50e3db73c3e7e81db99d5743541
Mirza Leka <mirza_376@hotmail.com>
2018-10-24 03:36:17 +0200

    contextPlayground

---
+++ Lesson12/contextPlayground.js
@@ -0,0 +1,27 @@
+
+const person = {
+    firstName: "Mirza",
+    lastName: "Leka",
+    fullName () {
+        return  `${this.firstName} ${this.lastName}`
+         }
+    };
+
+const definePerson = function (...attr) {
+    const [a, b] = attr;
+ console.log(`My name is ${this.fullName()}. I'm ${a} years old and I love ${b}.`); 
+}
+
+ definePerson.apply(person, [24, "JS"]);
+
+ const cama = {
+     firstName: "Ermin",
+     lastName: "Cama"
+ }
+
+ // We cam allow another object to access method fullName from person object
+ console.log(person.fullName.call(cama)); 
+
+ // difference between call and apply:
+// apply uses array as the second argument
+
