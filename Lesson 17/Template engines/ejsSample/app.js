67063f2ddb313019ed27d4ae14c2150b35219f69
Mirza Leka <mirza_376@hotmail.com>
2018-11-05 01:24:05 +0100

    Added views, embeded javascript and partials

--- Lesson 17/Template engines/ejsSample/app.js
+++ Lesson 17/Template engines/ejsSample/app.js
@@ -12,18 +12,41 @@ app.set("view engine", "ejs"); // what we want to set, which engine we want to t
 
 // When working with templating engines, we don't use sendFile method. Instead we use render method to render view
 
+
+// Note: As it turns out, by default express wants our views to be in the root JS30/views",
+// meaning it will be among other lessons and that's not what we want. 
+// So we'll need to tell him to relocate default folder
+
+app.set('views', __dirname + '/views'); // only for this case
+
+// another way to do this is to use path built in module
+/* 
+const path = require('path');
+app.set('views', path.join(__dirname, '/yourViewDirectory'));
+*/
+
 app.get('/', (req, res) => {
-    
+    res.render("home");
 });
 
 app.get('/contact', (req, res) => {
-    
+    res.render("contact");
 });
 
 app.get('/profile/:name', (req, res) => {
-    res.render("profile"); // we pass in the name of the view
+    
+    let data = {age: 24, job: "dev", hobbies: ["traveling", "movies", "music"]};
+
+    res.render("profile", {
+        person: req.params.name, // we're passing data from the url to a website
+        data: data
+    }); 
+
+    // we pass in the name of the view, dynamic content (object)
     // we don't need to specify file type, because we already did that in set view engine line
-    // we also don't need to use __dirname + path, because once again views folder is default folder and express will look there
+    // we also don't need to use __dirname + path, because once again views folder is default folder
+    // and express will look for views inside that folder
+    // and also we can pass some data using objects
 });
 
 
