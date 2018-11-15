3f1305b5b4c7062834dbc43ff7e1ed02950148bb
Mirza Leka <mirza_376@hotmail.com>
2018-11-15 19:51:51 +0100

    Added webpack jquery

--- Lesson 8/webpack/app.js
+++ Lesson 8/webpack/app.js
@@ -1,5 +1,10 @@
 import subtract, { square, sum, str } from './another';
 import divide from './singleExport'; // we are importing just default export
+import 'jquery';
+
+const _ = require('lodash');
+
+// import './styles/styles.css'; // we are importing our styles file into js
 
 const div = document.createElement('div');
 div.style.width = '120px';
@@ -30,3 +35,12 @@ document.body.appendChild(div);
 // with default export naming is not important. We can do it like below:
 
 // import anythingWeWant, { square, add} from './another' and it will work just fine
+
+$('body').css({
+  background: 'whitesmoke',
+  height: '100vh',
+  width: '100%',
+  overflowX: 'hidden'
+});
+
+console.log(_.isEmpty(''));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, '/Lesson 8/webpack/app.js'),
+  entry: path.join(__dirname, '/Lesson 8/webpack/app.js'), // main js file
   output: {
-    path: path.join(__dirname, '/Lesson 8/webpack/dist'),
+    path: path.join(__dirname, '/Lesson 8/webpack/dist'), // output destination
     filename: 'bundle.js'
   },
   mode: 'development',
@@ -12,10 +13,22 @@ module.exports = {
       loader: 'babel-loader',
       test: /\.js$/, // what file types we want to run with babel loader & check if file ends with .js
       exclude: /node_modules/ // we are excluding node modules
+    }, {
+      test: /\.css$/, // looking for any file that ends with .css
+      use: [ // we need multiple loaders, so we'll use "use" and setup array of loaders
+        'style-loader',
+        'css-loader' // we just installed these
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map', // finds error in original src. This src map can't be used in production
   devServer: { // starts server on port 8080 and autoreloads
     contentBase: path.join(__dirname, '/Lesson 8/webpack/dist') // static files location
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 };
