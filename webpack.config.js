dac53a962dc462240e2c10cc66289fa6c2d162db
Mirza Leka <mirza_376@hotmail.com>
2018-11-15 01:01:26 +0100

    Added webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,16 @@ module.exports = {
     path: path.join(__dirname, '/Lesson 8/webpack/dist'),
     filename: 'bundle.js'
   },
-  mode: 'production',
+  mode: 'development',
   module: {
     rules: [{
       loader: 'babel-loader',
       test: /\.js$/, // what file types we want to run with babel loader & check if file ends with .js
       exclude: /node_modules/ // we are excluding node modules
     }]
+  },
+  devtool: 'cheap-module-eval-source-map', // finds error in original src. This src map can't be used in production
+  devServer: { // starts server on port 8080 and autoreloads
+    contentBase: path.join(__dirname, '/Lesson 8/webpack/dist') // static files location
   }
 };
