b098f101473cc3bf9cdcd1859db6ca509b37cca9
Mirza Leka <mirza_376@hotmail.com>
2018-11-29 22:37:08 +0100

    Added GET ajax request. Fetching data from users.json file

--- Lesson 16/fileSystem playground/resources/playground.js
+++ Lesson 16/fileSystem playground/resources/playground.js
@@ -1,4 +1,37 @@
 
+window.onload = () => {
+  getUsers();
+};
+
+let users = '';
+
+function getUsers() {
+ 
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.open('GET', '/users', true); // getting data from that /users route
+  
+  xhttp.onload = function () {
+    if (this.status === 200) {
+      users = this.responseText; // we'll fill users variable with what is inside users.json file
+      users = JSON.parse(users); // what we get from server is always string, so we need to prase it into object
+
+      // Note: Arrays are type Object in Javascript
+      console.log(users);
+    } else if (this.status === 404) {
+      console.log('Route not found!');
+    }
+  };
+
+  xhttp.onerror = function () { // I'm using normal functions because arrow functions don't bind this keyword
+    console.log('Request failed!');
+  };
+  
+  xhttp.send();
+  
+
+}
+
 function User(username, age) {
   this.username = username;
   this.age = age;
@@ -6,8 +39,7 @@ function User(username, age) {
 
 document.querySelector('#saveBtn').addEventListener('click', save);
   
-const users = new Array();
-  
+
 function save() {
     
   const username = document.querySelector('#username').value;
