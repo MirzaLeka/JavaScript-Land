dff9bd8e4cfe7cbe03ebd1dedd2359ef5c619045
Mirza Leka <mirza_376@hotmail.com>
2019-03-06 23:59:08 +0100

    updating exp2

--- Lesson 1/DOM/Accessing Form elements/Exp2-formEvents.js
+++ Lesson 1/DOM/Accessing Form elements/Exp2-formEvents.js
@@ -1,9 +1,21 @@
 
 function funky(event) {
-    event.preventDefault();
+    // we pass event in our HTML
+
+    event.preventDefault(); // we call this function to prevent page reload on form submit
+  
+    // event.target is our HTML form we submitted 
+
+    console.log(event.target.children); // returns HTML collection of elements inside form ( input, input ,button )
+
+    // we can access value of first input ( inside form ) by collection index
+
+    console.log(event.target.children[0].value);
+
+    // we can also access value using "name" attribute we give to HTML input tags (username and password)
     
-    event.target.children[2].style.background = 'black';
-    
-    console.log(event.target.password.value);
+    console.log(event.target.username.value);
+
+    // both of these will give us value of first input field
   }
 
