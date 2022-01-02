6779cc2347392c7eab7e8458a6a49155fa54776e
Mirza Leka <mirza_376@hotmail.com>
2022-01-02 01:03:11 +0100

    Create count-occurrences.js

---
+++ Tips and Tricks/count-occurrences.js
@@ -0,0 +1,26 @@
+// Count occurrences in array
+
+const letters = ['A', 'B', 'C', 'A', 'B', 'B', 'C', 'D', 'A', 'A'];
+
+const collection = {};
+
+letters.forEach(letter => {
+
+  if (collection[letter]) {
+    collection[letter]++;
+  } else {
+    collection[letter] = 1;
+  }
+
+});
+
+console.log(collection);
+
+/*
+{
+  "A": 4,
+  "B": 3,
+  "C": 2,
+  "D": 1
+} 
+*/
